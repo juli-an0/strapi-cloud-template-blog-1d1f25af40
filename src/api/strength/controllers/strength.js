f41b43acf4b5ed28e7ec387f5bade337dc2d4ada
Julian Boldt <julian@familie-boldt.net>
2026-06-06 17:13:02 +0200

    Add Strength and Question content types

--- src/api/strength/controllers/strength.js
+++ src/api/strength/controllers/strength.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- *  category controller
+ * strength controller
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::category.category');
+module.exports = createCoreController('api::strength.strength');
